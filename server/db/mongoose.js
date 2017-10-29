4c4fb3a5c7f79e39f709de3ed28d12ccf29f2a24
Tom <tomutm124@gmail.com>
2017-10-29 12:35:07 +0800

    setip app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+const mongodbUrl = 'mongodb://pwsameasuser:pwsameasuser@ds137435.mlab.com:37435/node-todo-app'
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mongodbUrl);
 
 module.exports = {
   mongoose: mongoose
